decfd0e3572a0833cc2546310b07e16d3150d6bd
MykaelSalmarCosta <devmysco@gmail.com>
2025-11-15 19:11:35 -0300

    corrigindo bloqueio ao tentar acessar a api pelo frontend

--- src/api/mangadex.js
+++ src/api/mangadex.js
@@ -1,34 +1,5 @@
-const BASE_URL = "https://api.mangadex.org";
-
 export async function getManhwas() {
-  try {
-    const response = await fetch(
-    `${BASE_URL}/manga?limit=20&includes[]=cover_art&contentRating[]=safe&contentRating[]=safe`
-  );
-
-  const data = await response.json();
-
-  return data.data.map(item => {
-    const id = item.id;
-    const attributes = item.attributes;
-
-    const coverRel = item.relationships.find(rel => rel.type === "cover_art");
-    
-    
-    const coverFileName = coverRel?.attributes.fileName;
-    const coverUrl = coverFileName
-      ? `https://uploads.mangadex.org/covers/${id}/${coverFileName}`
-      : null;
-
-    return {
-      id,
-      title: attributes.title.en || attributes.title["en"] || "Título desconhecido",
-      description: attributes.description.en || attributes.description["en"] || "Sem descrição disponível",
-      cover: coverUrl,
-    };
-  });
-} catch (error) {
-    console.error("Erro ao buscar manhwas:", error);
-    return [];
-  }
+  const res = await fetch("/api/mangadex");
+  const data = await res.json();
+  return data.data;
 }
